88bcbc8975aa68f77bfda8e04b8c9dffbba70a9b
Makoto <rmt.makoto@gmail.com>
2019-08-26 22:50:01 -0300

    hideKeyboard

---
+++ Sens/Controller/ViewController + HideKeyboard.swift
@@ -0,0 +1,9 @@
+//
+//  ViewController + HideKeyboard.swift
+//  Sens
+//
+//  Created by Rodrigo Takumi on 26/08/19.
+//  Copyright © 2019 Bruno Cardoso Ambrosio. All rights reserved.
+//
+
+import Foundation
