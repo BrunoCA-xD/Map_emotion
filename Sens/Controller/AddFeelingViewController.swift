62c9c163afea5df1c017aba7e1362ff9d41eb5b9
Bruno C Ambrosio <bruno.car.ambrosio@gmail.com>
2019-08-27 19:42:48 -0300

    Tags Collectionview working well

--- Sens/Controller/AddFeelingViewController.swift
+++ Sens/Controller/AddFeelingViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class AddFeelingViewController: UIViewController, EmojiPickerViewBackButtonDelegate {
-
+class AddFeelingViewController: UIViewController, EmojiPickerViewBackButtonDelegate, RemoveDelegate{
+    
+    @IBOutlet weak var tagTextField: UITextField!
     @IBOutlet weak var textViewThoughts: UITextView!
     @IBOutlet weak var colorCollectionView: UICollectionView!
     @IBOutlet weak var tagCollectionView: UICollectionView!
+    @IBAction func addEmotionTagPressed(_ sender: Any) {
+        if let tagText = tagTextField.text{
+            var tag:EmotionTag = EmotionTag()
+            tag.tag = tagText
+            pin.tags.append(tag)
+            tagCollectionView.reloadData()
+        }
+    }
     @IBAction func selectEmojiPressed(_ sender: Any) {
         openCard()
     }
@@ -21,7 +30,7 @@ class AddFeelingViewController: UIViewController, EmojiPickerViewBackButtonDeleg
     var visualEffectView: UIVisualEffectView!
     var cardHeight: CGFloat = 0
     let cardHandleAreaHeight: CGFloat = 60
-    
+    var pin: EmotionPin = EmotionPin()
     var cellTagIds: [String] = []
     let cellIds = ["1cell","2cell","3cell","4cell","5cell","6cell","7cell","8cell","9cell","10cell"]
     
@@ -30,9 +39,13 @@ class AddFeelingViewController: UIViewController, EmojiPickerViewBackButtonDeleg
         super.viewDidLoad()
         hideKeyboardWhenTappedAround()
         setTextView()
+        
         // Do any additional setup after loading the view.
     }
-    
+    func removeButtonPressed(_ index: Int) {
+        pin.tags.remove(at: index)
+        tagCollectionView.reloadData()
+    }
     func backButton(_ emojiCode: String?) {
         
         UIView.animate(withDuration: 0.5, animations: {
@@ -74,6 +87,7 @@ class AddFeelingViewController: UIViewController, EmojiPickerViewBackButtonDeleg
 extension AddFeelingViewController: UICollectionViewDelegate {
         func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
             print("User tapped on \(cellIds[indexPath.row])")
+           
         }
     
 } // end extension AddFeelingViewController
@@ -84,26 +98,22 @@ extension AddFeelingViewController: UICollectionViewDataSource {
             if collectionView == self.colorCollectionView {
                 return cellIds.count
             } else {
-                return cellTagIds.count
+                return pin.tags.count
             }
         }
-        
-        //    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        //
-        //        return collectionView.dequeueReusableCell( withReuseIdentifier: cellIds[indexPath.item], for: indexPath)
-        //    }
+
         func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
             if collectionView == self.colorCollectionView {
                 let cellColor = collectionView.dequeueReusableCell(withReuseIdentifier: cellIds[indexPath.item], for: indexPath)
                 
                 return cellColor
             } else {
-                let cellTag = collectionView.dequeueReusableCell(withReuseIdentifier: "tagsCollectionView", for: indexPath)
-                let title = UILabel(frame: CGRect(x: 0, y: 0, width: cellTag.bounds.size.width, height: 40))
-                title.textColor = UIColor.black
-                title.textAlignment = .center
-                title.text = "T"
-                cellTag.contentView.addSubview(title)
+                let cellTag = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+                cellTag.removeDelegate = self
+                cellTag.index = indexPath.item
+                cellTag.labelEmotionCell.text = pin.tags[indexPath.item].tag
+                cellTag.layer.borderColor = UIColor.red.cgColor
+                cellTag.layer.borderWidth = 1
                 return cellTag
             }
         }
@@ -113,14 +123,23 @@ extension AddFeelingViewController: UICollectionViewDataSource {
 
 extension AddFeelingViewController: UICollectionViewDelegateFlowLayout {
         func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+            
             return 5
+            
         }
-        
+    
         func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-            
-            
-            let cellSizes = Array( repeatElement(CGSize(width:(collectionView.bounds.width - 45)/10, height:(collectionView.bounds.width - 45)/10), count: 10))
-            return cellSizes[indexPath.item]
+            if collectionView == self.colorCollectionView {
+                let cellSizes = Array( repeatElement(CGSize(width:(collectionView.bounds.width - 45)/10, height:(collectionView.bounds.width - 45)/10), count: 10))
+                return cellSizes[indexPath.item]
+            } else{
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+                var size:CGSize = cell.contentView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+//                size.height += 10
+//                size.width += 15
+                print("\(size)")
+                return size
+            }
         }
     
 } // end extension AddFeelingViewController
